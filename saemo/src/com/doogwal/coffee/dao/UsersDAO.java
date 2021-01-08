0284f92d132b5186b6c1065587672bdc214eaecf
E <etkl8888@naver.com>
2021-01-08 18:20:38 +0900

    2021 01 08 주성호

--- saemo/src/com/doogwal/coffee/dao/UsersDAO.java
+++ saemo/src/com/doogwal/coffee/dao/UsersDAO.java
@@ -10,6 +10,24 @@ import com.doogwal.coffee.vo.User;
 
 public class UsersDAO {
 
+	//로그인시 로그인할 수 있는 유저가 맞는지 확인하는
+	public static User selectLogin(User user) {
+		
+		SqlSession session = null;
+		
+		try {
+			session = SqlSessionUtil.getSession();
+			
+			return session.selectOne("users.selectLogin",user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			session.close();
+		}
+		
+		return null;
+	}
+	
 	public static List<User> selectList() {
 
 		SqlSession session = null;
